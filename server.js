62b9377da5061c7cbd015d7e0fe65b05dacc465d
Lucas Willering <lucas.willering@artificial-solutions.com>
2019-04-18 15:35:08 +0200

    Comments

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2019 Artificial Solutions. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const http = require('http');
 const express = require('express');
 const qs = require('querystring');
@@ -107,6 +123,9 @@ function sendTwilioMessage(teneoResponse, res) {
 
 function SessionHandler() {
 
+  // Map the Twilio CallSid id to the teneo engine session id. 
+  // This code keeps the map in memory, which is ok for testing purposes
+  // For production usage it is advised to make use of more resilient storage mechanisms like redis
   var sessionMap = new Map();
 
   return {
